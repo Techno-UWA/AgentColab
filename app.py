b8ff6ceac902badf544b4a12c4817f024efa6701
Techno-UWA <117506201+Techno-UWA@users.noreply.github.com>
2025-02-27 12:41:18 +0000

    Made simple functional Groq message test with Groq API. Not back and forth chat yet

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from groq import Groq
 import os
 
+# "Tell me a high fantasy, dungeons and dragons like adventure story. It must use immersive story telling and contain a wizard"
+
 client = Groq(# This is the default and can be omitted
 
     api_key=os.environ.get("GROQ_API_KEY"),)
     
 completion = client.chat.completions.create(
     model="deepseek-r1-distill-llama-70b",
-    messages=[],
+        messages=[
+        {
+            "role": "user",
+            "content": "Tell me a high fantasy, dungeons and dragons like adventure story. It must use immersive story telling and contain a wizard"
+        }
+    ],
     temperature=0.6,
     max_completion_tokens=4096,
     top_p=0.95,
